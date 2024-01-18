013ed5dc7d93c214ccbf85a1f27d3f9f9d7984d0
Ravi Teja Gutta <rteja1113@gmail.com>
2024-01-18 14:30:03 -0500

    separate out price page properties

---
+++ iex_app/scraping/price_page_properties.py
@@ -0,0 +1,18 @@
+import os
+
+from iex_app.common.constants import BASE_MARKET_URL
+
+
+class BasePricePageProperties:
+    PAGE_URL = BASE_MARKET_URL
+    NUM_COLS_IN_PRICE_TABLE = 1
+
+
+class DAMPricePageProperties(BasePricePageProperties):
+    PAGE_URL = os.path.join(BASE_MARKET_URL, "areaprice.aspx")
+    NUM_COLS_IN_PRICE_TABLE = 18
+
+
+class RTMPricePageProperties(BasePricePageProperties):
+    PAGE_URL = os.path.join(BASE_MARKET_URL, "rtm_areaprice.aspx")
+    NUM_COLS_IN_PRICE_TABLE = 19
