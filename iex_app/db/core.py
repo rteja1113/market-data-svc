78bc071d40b99b712a5cc4a779cbe3bea1489495
Ravi Teja Gutta <rteja1113@gmail.com>
2024-01-29 01:41:47 -0500

    create Session class using sessionmaker a SessionFactory

--- iex_app/db/core.py
+++ iex_app/db/core.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "postgresql+psycopg2://user:password@localhost:5432/mydatabase"
 
@@ -7,5 +8,5 @@ engine = create_engine(
     DATABASE_URL,
 )
 
-
+Session = sessionmaker(bind=engine)
 Base = declarative_base()
