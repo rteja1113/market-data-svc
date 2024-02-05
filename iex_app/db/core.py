5943d9d4ad29302c5d7f50b3c12dc7289c7b0bcd
Ravi Teja Gutta <rteja1113@gmail.com>
2024-02-04 21:48:02 -0500

    suppress warnings declarative_base import

--- iex_app/db/core.py
+++ iex_app/db/core.py
@@ -1,8 +1,7 @@
 import os
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
