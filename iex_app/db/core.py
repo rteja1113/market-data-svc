40e77861e8f731af4b5dbd13a17d340eff68e365
Ravi Teja Gutta <rteja1113@gmail.com>
2024-02-04 21:08:21 -0500

    no need default vals

--- iex_app/db/core.py
+++ iex_app/db/core.py
@@ -4,16 +4,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_USER = os.getenv("DB_USER", "")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "")
-DB_HOST = os.getenv("DB_HOST", "")
-DB_PORT = os.getenv("DB_PORT", "")
-DB_NAME = os.getenv("DB_NAME", "")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 SQLALCHEMY_DATABASE_URI = (
     f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
-ALEMBIC_REVISION_PATH = os.getenv("ALEMBIC_REVISION_PATH", "")
-ALEMBIC_INI_PATH = os.getenv("ALEMBIC_INI_PATH", "")
+ALEMBIC_REVISION_PATH = os.getenv("ALEMBIC_REVISION_PATH")
+ALEMBIC_INI_PATH = os.getenv("ALEMBIC_INI_PATH")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URI,
