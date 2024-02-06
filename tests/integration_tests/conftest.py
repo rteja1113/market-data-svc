c1402e8b4597446ea0dedfeb01d9cb4d1e8a8def
Ravi Teja Gutta <rteja1113@gmail.com>
2024-02-05 23:46:40 -0500

    move common data to conftest

--- tests/integration_tests/conftest.py
+++ tests/integration_tests/conftest.py
@@ -1,8 +1,13 @@
+import datetime
 import os.path
 
 import pytest
 from sqlalchemy_utils import database_exists, drop_database
 from starlette.config import environ
+
+from iex_app.api.models.pydantic_models import MARKETTYPE_TO_PRICE_PYD_MODEL_MAP
+from iex_app.common.constants import MARKET_TZ
+from iex_app.common.enums import Markets
 
 environ["DB_USER"] = "test_user"  # noqa
 environ["DB_PASSWORD"] = "test_password"  # noqa
@@ -38,3 +43,35 @@ def session(db):
     session.begin_nested()
     yield session
     session.rollback()
+
+
+@pytest.fixture
+def mock_datetime():
+    return MARKET_TZ.localize(datetime.datetime(2022, 1, 1))
+
+
+@pytest.fixture
+def mock_prices():
+    return [10.0] * 14
+
+
+@pytest.fixture
+def pyd_price_model(request, mock_datetime, mock_prices):
+    pyd_class = MARKETTYPE_TO_PRICE_PYD_MODEL_MAP.get(Markets[request.param])
+    return pyd_class(
+        settlement_period_start_datetime=mock_datetime,
+        a1_price_in_rs_per_mwh=mock_prices[0],
+        a2_price_in_rs_per_mwh=mock_prices[1],
+        e1_price_in_rs_per_mwh=mock_prices[2],
+        e2_price_in_rs_per_mwh=mock_prices[3],
+        n1_price_in_rs_per_mwh=mock_prices[4],
+        n2_price_in_rs_per_mwh=mock_prices[5],
+        n3_price_in_rs_per_mwh=mock_prices[6],
+        s1_price_in_rs_per_mwh=mock_prices[7],
+        s2_price_in_rs_per_mwh=mock_prices[8],
+        s3_price_in_rs_per_mwh=mock_prices[9],
+        w1_price_in_rs_per_mwh=mock_prices[10],
+        w2_price_in_rs_per_mwh=mock_prices[11],
+        w3_price_in_rs_per_mwh=mock_prices[12],
+        mcp_price_in_rs_per_mwh=mock_prices[13],
+    )
--- tests/integration_tests/test_basic_crud.py
+++ tests/integration_tests/test_basic_crud.py
@@ -1,53 +1,19 @@
-import datetime
-
 import pytest
 
 from iex_app.api.crud.basic_crud import MARKET_TO_DB_INSERTING_FN_MAP
 from iex_app.api.models.models import MARKETTYPE_TO_ORM_MAP
-from iex_app.api.models.pydantic_models import MARKETTYPE_TO_PRICE_PYD_MODEL_MAP
-from iex_app.common.constants import MARKET_TZ
 from iex_app.common.enums import Markets
 
 
-@pytest.fixture
-def mock_datetime():
-    return MARKET_TZ.localize(datetime.datetime(2022, 1, 1))
-
-
-@pytest.fixture
-def mock_prices():
-    return [10.0] * 14
-
-
-@pytest.fixture
-def pyd_model(request, mock_datetime, mock_prices):
-    pyd_class = MARKETTYPE_TO_PRICE_PYD_MODEL_MAP.get(Markets[request.param])
-    return pyd_class(
-        settlement_period_start_datetime=mock_datetime,
-        a1_price_in_rs_per_mwh=mock_prices[0],
-        a2_price_in_rs_per_mwh=mock_prices[1],
-        e1_price_in_rs_per_mwh=mock_prices[2],
-        e2_price_in_rs_per_mwh=mock_prices[3],
-        n1_price_in_rs_per_mwh=mock_prices[4],
-        n2_price_in_rs_per_mwh=mock_prices[5],
-        n3_price_in_rs_per_mwh=mock_prices[6],
-        s1_price_in_rs_per_mwh=mock_prices[7],
-        s2_price_in_rs_per_mwh=mock_prices[8],
-        s3_price_in_rs_per_mwh=mock_prices[9],
-        w1_price_in_rs_per_mwh=mock_prices[10],
-        w2_price_in_rs_per_mwh=mock_prices[11],
-        w3_price_in_rs_per_mwh=mock_prices[12],
-        mcp_price_in_rs_per_mwh=mock_prices[13],
-    )
-
-
 @pytest.mark.parametrize(
-    "pyd_model, price_type", [("DAM", "DAM"), ("RTM", "RTM")], indirect=["pyd_model"]
+    "pyd_price_model, price_type",
+    [("DAM", "DAM"), ("RTM", "RTM")],
+    indirect=["pyd_price_model"],
 )
-def test_inserting_records(session, pyd_model, price_type):
+def test_inserting_records(session, pyd_price_model, price_type):
     market_type_enum = Markets[price_type]
     row_inserting_fn = MARKET_TO_DB_INSERTING_FN_MAP.get(market_type_enum)
-    db_model = row_inserting_fn(session, pyd_model)
+    db_model = row_inserting_fn(session, pyd_price_model)
 
     # check instance type
     assert isinstance(db_model, MARKETTYPE_TO_ORM_MAP.get(market_type_enum))
