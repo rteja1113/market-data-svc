d0d4178c6354824de8ec5551d4dc3478d8273762
Ravi Teja Gutta <rteja1113@gmail.com>
2024-02-10 13:25:39 -0500

    test for manual migration

---
+++ tests/integration_tests/test_manual_data_migration.py
@@ -0,0 +1,24 @@
+import pytest
+from click.testing import CliRunner
+
+from iex_app.api.models.models import MARKETTYPE_TO_ORM_MAP
+from iex_app.common.enums import Markets
+from iex_app.db.manual_data_migration import export_json_price_data_into_db
+
+
+@pytest.mark.parametrize(
+    "json_path, price_type",
+    [
+        ("./tests/integration_tests/json_data/dam_prices.json", "DAM"),
+        ("./tests/integration_tests/json_data/rtm_prices.json", "RTM"),
+    ],
+)
+def test_export_json_price_data_into_db(monkeypatch, session, json_path, price_type):
+    monkeypatch.setattr("iex_app.db.manual_data_migration.session", session)
+    runner = CliRunner()
+    _ = runner.invoke(export_json_price_data_into_db, [json_path, price_type])
+    market_type_enum = Markets[price_type]
+
+    # check that the record was inserted into the db
+    orm_instance = session.query(MARKETTYPE_TO_ORM_MAP.get(market_type_enum)).first()
+    assert isinstance(orm_instance, MARKETTYPE_TO_ORM_MAP.get(market_type_enum))
