150bf19fc7e79c8b31fb6713454657c5fc1f6e55
Ravi Teja Gutta <rteja1113@gmail.com>
2024-02-26 00:10:35 -0500

    rename DownloadWindow to TimeFrame

--- iex_app/common/models.py
+++ iex_app/common/models.py
@@ -5,9 +5,9 @@ import datetime
 from pydantic import BaseModel, model_validator
 
 
-class DownloadWindow(BaseModel):
+class TimeFrame(BaseModel):
     """
-    A class that describes the download window for the price.
+    A class that describes the Time Frame for the price.
     start_datetime should be less than end_datetime
     """
 
@@ -21,8 +21,8 @@ class DownloadWindow(BaseModel):
 
 
 if __name__ == "__main__":
-    download_window = DownloadWindow(
+    download_window = TimeFrame(
         start_datetime=datetime.datetime(2020, 1, 1),
-        end_datetime=datetime.datetime(2021, 1, 1),
+        end_datetime=datetime.datetime(2019, 1, 1),
     )
     print(download_window)
