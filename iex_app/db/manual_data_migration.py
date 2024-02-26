c694a1b171f99ee6c5547bef460977ac512a4d20
Ravi Teja Gutta <rteja1113@gmail.com>
2024-02-26 00:17:34 -0500

    bug in manual migration script

--- iex_app/db/manual_data_migration.py
+++ iex_app/db/manual_data_migration.py
@@ -1,15 +1,14 @@
 import datetime
 import json
 import logging
-from typing import Callable, Union
 
 import click
 from dateutil import parser
 
 from iex_app.api.crud.basic_crud import MARKET_TO_DB_INSERTING_FN_MAP
 from iex_app.api.models.pydantic_models import (
+    MARKETTYPE_TO_PRICE_PYD_MODEL_MAP,
     BasePointInTimePriceData,
-    DAMPointInTimePriceData,
 )
 from iex_app.common.constants import MARKET_TZ
 from iex_app.common.enums import Markets
@@ -32,15 +31,19 @@ def _convert_to_market_tz(datetime_string: str) -> datetime.datetime:
     return market_tz_datetime
 
 
-def _convert_dict_to_pyd(json_data: list[dict]) -> list[BasePointInTimePriceData]:
+def _convert_dict_to_pyd(
+    json_data: list[dict], price_enum: Markets
+) -> list[BasePointInTimePriceData]:
     pit_pyd = []
     for row in json_data:
         market_tz_datetime = _convert_to_market_tz(
             row["settlement_period_start_datetime"]
         )
         row["settlement_period_start_datetime"] = market_tz_datetime
-        pit_pyd.append(DAMPointInTimePriceData(**row))
-    return pit_pyd
+        pyd_class = MARKETTYPE_TO_PRICE_PYD_MODEL_MAP[price_enum]
+        pyd_instance = pyd_class(**row)
+        pit_pyd.append(pyd_instance)
+    return pit_pyd  # type: ignore
 
 
 def _if_path_matches_price_type(json_path: str, price_type: str) -> bool:
@@ -59,20 +62,14 @@ def _if_path_matches_price_type(json_path: str, price_type: str) -> bool:
     return True
 
 
-def _get_row_inserting_fn_from_price_type(price_type: str) -> Union[Callable | None]:
-    price_enum = Markets[price_type]
-    row_inserting_fn = MARKET_TO_DB_INSERTING_FN_MAP.get(price_enum)
-    return row_inserting_fn
-
-
 @click.command()
 @click.option("--json_path", type=str)
 @click.option("--price_type", type=click.Choice(["DAM", "RTM"], case_sensitive=False))
 def export_json_price_data_into_db(json_path: str, price_type: str) -> None:
     if _if_path_matches_price_type(json_path, price_type):
         price_pit_rows = _load_price_data_from_json(json_path)
-        pit_pyd_models = _convert_dict_to_pyd(price_pit_rows)
         price_enum = Markets[price_type]
+        pit_pyd_models = _convert_dict_to_pyd(price_pit_rows, price_enum)
         row_inserting_fn = MARKET_TO_DB_INSERTING_FN_MAP.get(price_enum)
         if row_inserting_fn is None:
             raise ValueError(f"Invalid price type: {price_type}")
