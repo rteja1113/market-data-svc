1ecb9f25b941e4d3e84ad87252681cf36d56afa7
Ravi Teja Gutta <rteja1113@gmail.com>
2024-03-24 00:07:07 -0400

    tests for data getters

--- tests/integration_tests/test_crud.py
+++ tests/integration_tests/test_crud.py
@@ -1,7 +1,13 @@
+import datetime
+
 import pytest
 
 from src.common.enums import Markets
-from src.marketdata.crud import MARKET_TO_DB_INSERTING_FN_MAP
+from src.common.models import TimeFrame
+from src.marketdata.crud import (
+    MARKET_TO_DB_GETTING_FN_MAP,
+    MARKET_TO_DB_INSERTING_FN_MAP,
+)
 from src.marketdata.models import MARKETTYPE_TO_ORM_MAP
 
 
@@ -21,3 +27,23 @@ def test_inserting_records(session, pyd_price_model, price_type):
     assert (
         session.query(MARKETTYPE_TO_ORM_MAP.get(market_type_enum)).first() == db_model
     )
+
+
+@pytest.mark.parametrize(
+    "pyd_price_model, price_type",
+    [("DAM", "DAM"), ("RTM", "RTM")],
+    indirect=["pyd_price_model"],
+)
+def test_getting_records(session, pyd_price_model, price_type):
+    market_type_enum = Markets[price_type]
+    row_getting_fn = MARKET_TO_DB_GETTING_FN_MAP.get(market_type_enum)
+    time_frame = TimeFrame(
+        start_datetime=datetime.datetime(2000, 1, 1),
+        end_datetime=datetime.datetime(2050, 1, 1),
+    )
+    db_models = row_getting_fn(session, time_frame)
+
+    # check instance type
+    for db_model in db_models:
+        assert isinstance(db_model, MARKETTYPE_TO_ORM_MAP.get(market_type_enum))
+        # check by querying the migrations
