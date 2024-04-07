7856a0bc2052cd2359251005ef21c0360d1f883b
Ravi Teja Gutta <rteja1113@gmail.com>
2024-04-07 11:19:05 -0400

    using mappers

--- src/marketdata/router.py
+++ src/marketdata/router.py
@@ -1,19 +1,16 @@
 import datetime
-import functools
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Path, Query
+import fastapi
+from fastapi import APIRouter, Depends, Query
+from starlette import status as StarletteStatus
 
 from src.common import logging_utils
-from src.common.enums import Markets
 from src.common.utils import convert_naive_datetime_in_utc_to_ist
 from src.database import Session  # noqa
-from src.marketdata.crud import MARKET_TO_DB_GETTING_FN_MAP
-from src.marketdata.router_utils import (
-    _convert_orm_model_to_pydantic_model,
-    _validate_and_convert_datetime_params,
-)
-from src.marketdata.schemas import BasePointInTimePriceData
+from src.marketdata.crud import get_dam_price_records, get_rtm_price_records
+from src.marketdata.router_utils import _convert_datetime_query_params_to_time_frame
+from src.marketdata.schemas import DAMPointInTimePriceData, RTMPointInTimePriceData
 
 logger = logging_utils.create_logger(__name__)
 
@@ -29,24 +26,28 @@ def get_db_session():
         db_session.close()
 
 
-@router.get("/{market_type}")
-def read_price_records(
-    market_type: Annotated[Markets, Path(description="Market type (dam or rtm)")],
+@router.get("/dam")
+def read_dam_price_records(
     start_datetime_str: Annotated[str, Query(alias="start_datetime")],
     end_datetime_str: Annotated[str, Query(alias="end_datetime")],
     db_session: Annotated[Session, Depends(get_db_session)],
-) -> list[BasePointInTimePriceData]:
+) -> list[DAMPointInTimePriceData]:
     """
-    Fetches the price records for the given time frame and market type
+    Fetches the dam price records for the given time frame
     """
-    time_frame = _validate_and_convert_datetime_params(
-        start_datetime_str, end_datetime_str
-    )
+    try:
+        time_frame = _convert_datetime_query_params_to_time_frame(
+            start_datetime_str, end_datetime_str
+        )
+    except ValueError as e:
+        logger.error(f"Error while converting datetime query params: {e}")
+        raise fastapi.HTTPException(
+            status_code=StarletteStatus.HTTP_400_BAD_REQUEST,
+            detail="Invalid datetime format",
+        )
 
-    price_records = MARKET_TO_DB_GETTING_FN_MAP[market_type](db_session, time_frame)
-    orm_to_pydantic_converter = functools.partial(
-        _convert_orm_model_to_pydantic_model, market_type=market_type
-    )
+    price_records = get_dam_price_records(db_session, time_frame)
+
     price_pyd_models = []
     for price_record in price_records:
         ist_dt = convert_naive_datetime_in_utc_to_ist(
@@ -54,8 +55,74 @@ def read_price_records(
                 price_record.settlement_period_start_timestamp
             )
         )
-        price_pyd_model = orm_to_pydantic_converter(
-            settlement_period_start_datetime=ist_dt, orm_instance=price_record
+        price_pyd_model = DAMPointInTimePriceData(
+            settlement_period_start_datetime=ist_dt,
+            a1_price_in_rs_per_mwh=price_record.a1_price_in_rs_per_mwh,
+            a2_price_in_rs_per_mwh=price_record.a2_price_in_rs_per_mwh,
+            e1_price_in_rs_per_mwh=price_record.e1_price_in_rs_per_mwh,
+            e2_price_in_rs_per_mwh=price_record.e2_price_in_rs_per_mwh,
+            n1_price_in_rs_per_mwh=price_record.n1_price_in_rs_per_mwh,
+            n2_price_in_rs_per_mwh=price_record.n2_price_in_rs_per_mwh,
+            n3_price_in_rs_per_mwh=price_record.n3_price_in_rs_per_mwh,
+            s1_price_in_rs_per_mwh=price_record.s1_price_in_rs_per_mwh,
+            s2_price_in_rs_per_mwh=price_record.s2_price_in_rs_per_mwh,
+            s3_price_in_rs_per_mwh=price_record.s3_price_in_rs_per_mwh,
+            w1_price_in_rs_per_mwh=price_record.w1_price_in_rs_per_mwh,
+            w2_price_in_rs_per_mwh=price_record.w2_price_in_rs_per_mwh,
+            w3_price_in_rs_per_mwh=price_record.w3_price_in_rs_per_mwh,
+            mcp_price_in_rs_per_mwh=price_record.mcp_price_in_rs_per_mwh,
         )
+
+        price_pyd_models.append(price_pyd_model)
+    return price_pyd_models
+
+
+@router.get("/rtm")
+def read_rtm_price_records(
+    start_datetime_str: Annotated[str, Query(alias="start_datetime")],
+    end_datetime_str: Annotated[str, Query(alias="end_datetime")],
+    db_session: Annotated[Session, Depends(get_db_session)],
+) -> list[RTMPointInTimePriceData]:
+    """
+    Fetches rtm price records for the given time frame
+    """
+    try:
+        time_frame = _convert_datetime_query_params_to_time_frame(
+            start_datetime_str, end_datetime_str
+        )
+    except ValueError as e:
+        logger.error(f"Error while converting datetime query params: {e}")
+        raise fastapi.HTTPException(
+            status_code=StarletteStatus.HTTP_400_BAD_REQUEST,
+            detail="Invalid datetime format",
+        )
+
+    price_records = get_rtm_price_records(db_session, time_frame)
+
+    price_pyd_models = []
+    for price_record in price_records:
+        ist_dt = convert_naive_datetime_in_utc_to_ist(
+            datetime.datetime.utcfromtimestamp(
+                price_record.settlement_period_start_timestamp
+            )
+        )
+        price_pyd_model = RTMPointInTimePriceData(
+            settlement_period_start_datetime=ist_dt,
+            a1_price_in_rs_per_mwh=price_record.a1_price_in_rs_per_mwh,
+            a2_price_in_rs_per_mwh=price_record.a2_price_in_rs_per_mwh,
+            e1_price_in_rs_per_mwh=price_record.e1_price_in_rs_per_mwh,
+            e2_price_in_rs_per_mwh=price_record.e2_price_in_rs_per_mwh,
+            n1_price_in_rs_per_mwh=price_record.n1_price_in_rs_per_mwh,
+            n2_price_in_rs_per_mwh=price_record.n2_price_in_rs_per_mwh,
+            n3_price_in_rs_per_mwh=price_record.n3_price_in_rs_per_mwh,
+            s1_price_in_rs_per_mwh=price_record.s1_price_in_rs_per_mwh,
+            s2_price_in_rs_per_mwh=price_record.s2_price_in_rs_per_mwh,
+            s3_price_in_rs_per_mwh=price_record.s3_price_in_rs_per_mwh,
+            w1_price_in_rs_per_mwh=price_record.w1_price_in_rs_per_mwh,
+            w2_price_in_rs_per_mwh=price_record.w2_price_in_rs_per_mwh,
+            w3_price_in_rs_per_mwh=price_record.w3_price_in_rs_per_mwh,
+            mcp_price_in_rs_per_mwh=price_record.mcp_price_in_rs_per_mwh,
+        )
+
         price_pyd_models.append(price_pyd_model)
     return price_pyd_models
--- tests/integration_tests/test_router_utils.py
+++ tests/integration_tests/test_router_utils.py
@@ -4,19 +4,11 @@ import pytest
 from fastapi import HTTPException
 
 from src.common.constants import MARKET_TZ
-from src.common.enums import Markets
 from src.common.models import TimeFrame
-from src.marketdata.models import (
-    BasePointInTimePriceDataDb,
-    DAMPointInTimePriceDataDb,
-    RTMPointInTimePriceDataDb,
-)
 from src.marketdata.router_utils import (
-    _convert_orm_model_to_pydantic_model,
+    _convert_datetime_query_params_to_time_frame,
     _convert_string_to_datetime,
-    _validate_and_convert_datetime_params,
 )
-from src.marketdata.schemas import DAMPointInTimePriceData, RTMPointInTimePriceData
 
 
 def test__convert_string_to_datetime_with_valid_input():
@@ -33,7 +25,9 @@ def test__validate_and_convert_datetime_params_with_valid_input():
         end_datetime=MARKET_TZ.localize(datetime(2022, 1, 2, 0, 0, 0)),
     )
     assert (
-        _validate_and_convert_datetime_params(start_datetime_str, end_datetime_str)
+        _convert_datetime_query_params_to_time_frame(
+            start_datetime_str, end_datetime_str
+        )
         == expected_time_frame
     )
 
@@ -42,78 +36,6 @@ def test__validate_and_convert_datetime_params_with_invalid_dates():
     start_datetime_str = "2022-01-02 00:00:00"
     end_datetime_str = "2022-01-01 00:00:00"
     with pytest.raises(HTTPException):
-        _validate_and_convert_datetime_params(start_datetime_str, end_datetime_str)
-
-
-def test__convert_orm_model_to_pydantic_model_with_dam_instance():
-    settlement_period_start_datetime = datetime(2022, 1, 1, 0, 0, 0)
-    orm_instance = DAMPointInTimePriceDataDb(
-        settlement_period_start_timestamp=settlement_period_start_datetime.timestamp(),
-        a1_price_in_rs_per_mwh=100,
-        a2_price_in_rs_per_mwh=200,
-        e1_price_in_rs_per_mwh=300,
-        e2_price_in_rs_per_mwh=400,
-        n1_price_in_rs_per_mwh=500,
-        n2_price_in_rs_per_mwh=600,
-        n3_price_in_rs_per_mwh=700,
-        s1_price_in_rs_per_mwh=800,
-        s2_price_in_rs_per_mwh=900,
-        s3_price_in_rs_per_mwh=1000,
-        w1_price_in_rs_per_mwh=1100,
-        w2_price_in_rs_per_mwh=1200,
-        w3_price_in_rs_per_mwh=1300,
-        mcp_price_in_rs_per_mwh=1400,
-    )
-    result = _convert_orm_model_to_pydantic_model(
-        settlement_period_start_datetime, orm_instance, Markets.DAM
-    )
-    assert isinstance(result, DAMPointInTimePriceData)
-
-
-def test__convert_orm_model_to_pydantic_model_with_rtm_instance():
-    settlement_period_start_datetime = datetime(2022, 1, 1, 0, 0, 0)
-    orm_instance = RTMPointInTimePriceDataDb(
-        settlement_period_start_timestamp=settlement_period_start_datetime.timestamp(),
-        a1_price_in_rs_per_mwh=100,
-        a2_price_in_rs_per_mwh=200,
-        e1_price_in_rs_per_mwh=300,
-        e2_price_in_rs_per_mwh=400,
-        n1_price_in_rs_per_mwh=500,
-        n2_price_in_rs_per_mwh=600,
-        n3_price_in_rs_per_mwh=700,
-        s1_price_in_rs_per_mwh=800,
-        s2_price_in_rs_per_mwh=900,
-        s3_price_in_rs_per_mwh=1000,
-        w1_price_in_rs_per_mwh=1100,
-        w2_price_in_rs_per_mwh=1200,
-        w3_price_in_rs_per_mwh=1300,
-        mcp_price_in_rs_per_mwh=1400,
-    )
-    result = _convert_orm_model_to_pydantic_model(
-        settlement_period_start_datetime, orm_instance, Markets.RTM
-    )
-    assert isinstance(result, RTMPointInTimePriceData)
-
-
-def test__convert_orm_model_to_pydantic_model_with_invalid_market():
-    settlement_period_start_datetime = datetime(2022, 1, 1, 0, 0, 0)
-    orm_instance = BasePointInTimePriceDataDb(
-        a1_price_in_rs_per_mwh=100,
-        a2_price_in_rs_per_mwh=200,
-        e1_price_in_rs_per_mwh=300,
-        e2_price_in_rs_per_mwh=400,
-        n1_price_in_rs_per_mwh=500,
-        n2_price_in_rs_per_mwh=600,
-        n3_price_in_rs_per_mwh=700,
-        s1_price_in_rs_per_mwh=800,
-        s2_price_in_rs_per_mwh=900,
-        s3_price_in_rs_per_mwh=1000,
-        w1_price_in_rs_per_mwh=1100,
-        w2_price_in_rs_per_mwh=1200,
-        w3_price_in_rs_per_mwh=1300,
-        mcp_price_in_rs_per_mwh=1400,
-    )
-    with pytest.raises(ValueError):
-        _convert_orm_model_to_pydantic_model(
-            settlement_period_start_datetime, orm_instance, "invalid_market"
+        _convert_datetime_query_params_to_time_frame(
+            start_datetime_str, end_datetime_str
         )
