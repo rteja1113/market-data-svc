fcf70df3900f57cd35899610eb6e23f6d5633229
Ravi Teja Gutta <rteja1113@gmail.com>
2024-04-07 16:28:21 -0400

    unit test fixes

--- tests/integration_tests/test_router_utils.py
+++ tests/integration_tests/test_router_utils.py
@@ -1,41 +1,39 @@
 from datetime import datetime
 
 import pytest
-from fastapi import HTTPException
 
 from src.common.constants import MARKET_TZ
 from src.common.models import TimeFrame
-from src.marketdata.router_utils import (
-    _convert_string_to_datetime,
-    convert_datetime_query_params_to_time_frame,
-)
+from src.marketdata.router_utils import convert_datetime_query_params_to_time_frame
 
 
-def test__convert_string_to_datetime_with_valid_input():
-    datetime_string = "2022-01-01 00:00:00"
-    expected_datetime = MARKET_TZ.localize(datetime(2022, 1, 1, 0, 0, 0))
-    assert _convert_string_to_datetime(datetime_string) == expected_datetime
+@pytest.fixture
+def start_datetime_string():
+    return "2022-01-01 00:00:00"
 
 
-def test__validate_and_convert_datetime_params_with_valid_input():
-    start_datetime_str = "2022-01-01 00:00:00"
-    end_datetime_str = "2022-01-02 00:00:00"
+@pytest.fixture
+def end_datetime_string():
+    return "2022-01-01 23:00:00"
+
+
+def test_convert_datetime_query_params_to_time_frame_valid_input(
+    start_datetime_string, end_datetime_string
+):
+    actual_time_frame = convert_datetime_query_params_to_time_frame(
+        start_datetime_string, end_datetime_string
+    )
     expected_time_frame = TimeFrame(
         start_datetime=MARKET_TZ.localize(datetime(2022, 1, 1, 0, 0, 0)),
-        end_datetime=MARKET_TZ.localize(datetime(2022, 1, 2, 0, 0, 0)),
+        end_datetime=MARKET_TZ.localize(datetime(2022, 1, 1, 23, 0, 0)),
     )
-    assert (
-        convert_datetime_query_params_to_time_frame(
-            start_datetime_str, end_datetime_str
-        )
-        == expected_time_frame
-    )
+    assert actual_time_frame == expected_time_frame
 
 
-def test__validate_and_convert_datetime_params_with_invalid_dates():
-    start_datetime_str = "2022-01-02 00:00:00"
-    end_datetime_str = "2022-01-01 00:00:00"
-    with pytest.raises(HTTPException):
+def test_convert_datetime_query_params_to_time_frame_invalid_input(
+    start_datetime_string, end_datetime_string
+):
+    with pytest.raises(ValueError):
         convert_datetime_query_params_to_time_frame(
-            start_datetime_str, end_datetime_str
+            end_datetime_string, start_datetime_string
         )
