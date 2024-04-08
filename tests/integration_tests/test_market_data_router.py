ca240cc62d14162c2f19545b1b432690054a48c5
Ravi Teja Gutta <rteja1113@gmail.com>
2024-04-08 00:32:38 -0400

    make inserting row a fixture

--- tests/integration_tests/test_market_data_router.py
+++ tests/integration_tests/test_market_data_router.py
@@ -4,17 +4,24 @@ from src.common.enums import Markets
 from src.marketdata.crud import MARKET_TO_DB_INSERTING_FN_MAP
 
 
+@pytest.fixture
+def insert_row(session, pyd_price_model, price_type):
+    market_type_enum = Markets[price_type]
+    row_inserting_fn = MARKET_TO_DB_INSERTING_FN_MAP.get(market_type_enum)
+    _ = row_inserting_fn(session, pyd_price_model)
+
+
 @pytest.mark.parametrize(
     "pyd_price_model, price_type",
     [("DAM", "DAM"), ("RTM", "RTM")],
     indirect=["pyd_price_model"],
 )
 def test_read_price_records_valid_requests(
-    client, session, pyd_price_model, price_type
+    client, session, pyd_price_model, price_type, insert_row
 ):
-    market_type_enum = Markets[price_type]
-    row_inserting_fn = MARKET_TO_DB_INSERTING_FN_MAP.get(market_type_enum)
-    _ = row_inserting_fn(session, pyd_price_model)
+    # market_type_enum = Markets[price_type]
+    # row_inserting_fn = MARKET_TO_DB_INSERTING_FN_MAP.get(market_type_enum)
+    # _ = row_inserting_fn(session, pyd_price_model)
     response = client.get(
         f"/marketdata/{price_type.lower()}?"
         f"start_datetime=2000-01-01 00:00:00&end_datetime=2050-01-01 01:00:00"
@@ -31,11 +38,11 @@ def test_read_price_records_valid_requests(
     indirect=["pyd_price_model"],
 )
 def test_read_price_records_invalid_requests(
-    client, session, pyd_price_model, price_type
+    client, session, pyd_price_model, price_type, insert_row
 ):
-    market_type_enum = Markets[price_type]
-    row_inserting_fn = MARKET_TO_DB_INSERTING_FN_MAP.get(market_type_enum)
-    _ = row_inserting_fn(session, pyd_price_model)
+    # market_type_enum = Markets[price_type]
+    # row_inserting_fn = MARKET_TO_DB_INSERTING_FN_MAP.get(market_type_enum)
+    # _ = row_inserting_fn(session, pyd_price_model)
     response = client.get(
         f"/marketdata/{price_type.lower()}?"
         f"start_datetime=2000-01-01T00:00:00&end_datetime=2050-01-01T01:00:00"
