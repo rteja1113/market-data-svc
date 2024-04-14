532d5e5246d6a05235becfd44681c42b1bf6471e
Ravi Teja Gutta <rteja1113@gmail.com>
2024-04-14 16:23:20 -0400

    add str to datetime converter

--- src/common/models.py
+++ src/common/models.py
@@ -3,6 +3,9 @@ from __future__ import annotations
 import datetime
 
 from pydantic import BaseModel, model_validator
+from pydantic.functional_validators import field_validator
+
+from src.common.constants import MARKET_TZ
 
 
 class TimeFrame(BaseModel):
@@ -19,10 +22,17 @@ class TimeFrame(BaseModel):
         if self.start_datetime > self.end_datetime:
             raise ValueError("start_datetime should be less than end_datetime")
 
+    @field_validator("start_datetime", "end_datetime", mode="before")
+    def convert_to_ist(cls, value):
+        if isinstance(value, str):
+            return MARKET_TZ.localize(
+                datetime.datetime.strptime(value, "%Y-%m-%d %H:%M:%S")
+            )
+
 
 if __name__ == "__main__":
     download_window = TimeFrame(
         start_datetime=datetime.datetime(2020, 1, 1),
-        end_datetime=datetime.datetime(2019, 1, 1),
+        end_datetime=datetime.datetime(2020, 1, 2),
     )
     print(download_window)
