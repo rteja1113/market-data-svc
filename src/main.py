ed58c2b8f4c70cd4e52f2da423096bee9b28bc5a
rteja1113 <rteja1113@gmail.com>
2024-04-27 23:07:00 -0400

    dont create databse on app startup

--- src/main.py
+++ src/main.py
@@ -1,21 +1,18 @@
 import uvicorn
 from fastapi import FastAPI
-from sqlalchemy_utils import create_database, database_exists
 
 import src.marketdata.router
-from src.database import engine
-from src.manage import apply_migrations, wait_for_postgres
+from src.common import logging_utils
+from src.manage import wait_for_postgres
 
 app = FastAPI()
 app.include_router(src.marketdata.router.router)
+logger = logging_utils.create_logger(__name__)
 
 
 @app.on_event("startup")
 async def create_database_and_apply_migrations():
     wait_for_postgres()
-    if not database_exists(str(engine.url)):
-        create_database(str(engine.url))
-    apply_migrations()
 
 
 @app.get("/")
